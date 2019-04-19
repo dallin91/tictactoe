6346fdae2a2c14bbc138655ea8ab7052b3a07db6
dallin91 <dallinreeves@gmail.com>
2019-04-19 13:13:33 -0500

    fixed bug declaring stalemate after first move

--- tictactoe.py
+++ tictactoe.py
@@ -44,14 +44,14 @@ def win_check(board, mark):
     '''
     checks for a winner
     '''
-    return ((board[7] == board[8] == board[9] or
-            board[4] == board[5] == board[6] or
-            board[1] == board[2] == board[3] or
-            board[1] == board[4] == board[7] or
-            board[2] == board[5] == board[8] or
-            board[3] == board[6] == board[9] or
-            board[1] == board[5] == board[9] or
-            board[7] == board[5] == board[3]))
+    return ((board[7] == mark and board[8] == mark and board[9] == mark or
+            board[4] == mark and board[5] == mark and board[6] == mark or
+            board[1] == mark and board[2] == mark and board[3] == mark or
+            board[1] == mark and board[4] == mark and board[7] == mark or
+            board[2] == mark and board[5] == mark and board[8] == mark or
+            board[3] == mark and board[6] == mark and board[9] == mark or
+            board[1] == mark and board[5] == mark and board[9] == mark or
+            board[7] == mark and board[5] == mark and board[3] == mark))
 
 import random
 
@@ -77,7 +77,7 @@ def full_board_check(board):
     for i in range(1,10):
         if space_check(board, i):
             return False
-        return True
+    return True
 
 def player_choice(board):
     '''
@@ -95,3 +95,53 @@ def replay():
     play_again = input('Would you like to play again? (y/n) ')
     return play_again.lower() == 'y'
 
+print('Welcome to Tic Tac Toe!')
+
+while True:
+    theBoard = [' '] * 10
+    player1_marker, player2_marker = player_input()
+    turn = choose_first()
+    print(turn + " will go first")
+    
+    play_game = input('Are you ready to play? (yes or no) ')
+    
+    if play_game.lower()[0] == 'y':
+        game_on = True
+    else:
+        game_on = False
+
+    while game_on:
+        if turn == 'Player 1':
+            display_board(theBoard)
+            position = player_choice(theBoard)
+            place_marker(theBoard, player1_marker, position)
+            
+            if win_check(theBoard, player1_marker):
+                display_board(theBoard)
+                print("PLAYER 1 HAS WON")
+                game_on = False
+            else:
+                if full_board_check(theBoard):
+                    display_board(theBoard)
+                    print('STALEMATE')
+                    break
+                else:
+                    turn = 'Player 2'
+        else:
+            display_board(theBoard)
+            position = player_choice(theBoard)
+            place_marker(theBoard, player2_marker, position)
+            
+            if win_check(theBoard, player2_marker):
+                display_board(theBoard)
+                print("PLAYER 2 HAS WON")
+                game_on = False
+            else:
+                if full_board_check(theBoard):
+                    display_board(theBoard)
+                    print('STALEMATE')
+                    break
+                else:
+                    turn = 'Player 1'
+    if not replay():
+        break
